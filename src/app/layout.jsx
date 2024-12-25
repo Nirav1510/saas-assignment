8b0416167e290bd6806765cd6f873778616d3d8d
Nirav Gusai <gusainirav1@gmail.com>
2024-12-25 22:30:02 +0530

    prettier configuration

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,28 +1,25 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "Saas Assignment",
-  description: "Assignment submitted by Nirav Gusai",
+	title: 'Saas Assignment',
+	description: 'Assignment submitted by Nirav Gusai',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+		</html>
+	);
 }
